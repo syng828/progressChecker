6e34aab651a76075f12dece02e0987ffb3ccd81d
syng828 <syng828@gmail.com>
2023-02-12 22:44:11 -0800

    Class creation
    Created four classes: main, task which has topics, task which has questions, and question. Not done with methods.

---
+++ Question.java
@@ -0,0 +1,29 @@
+
+
+public class Question {
+    private String question; 
+    private String answer; 
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Question(String q, String a) { 
+        question = q; 
+        answer = a; 
+    }
+    
+
+}
---
+++ Task.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+
+public class Task {
+    private ArrayList<Topics> top;
+}
